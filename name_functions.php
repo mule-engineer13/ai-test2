08d6fc5e22628f518c3e83d454d1c2279068a6a8
mule-engineer13 <112328461+mule-engineer13@users.noreply.github.com>
2025-05-21 18:16:29 +0900

    Delete name_functions.php

--- name_functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-function validateName(string $name): void
-{
-    // 半角英数字・記号のみ許可（全角・日本語不可）
-    if (!preg_match('/^[a-zA-Z0-9!"#$%&\'()*+,\-\.\/:;<=>?@\[\\\]^_`{|}~]+$/', $name)) {
-        throw new InvalidArgumentException('名前には英数字および記号のみ使用可能です。');
-    }
-}
