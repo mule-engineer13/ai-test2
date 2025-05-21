d6f4b73dfdc270e0cb1929ab08f500f4b6e427a1
mule-engineer13 <112328461+mule-engineer13@users.noreply.github.com>
2025-05-21 18:20:10 +0900

    Delete image_functions.php

--- image_functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-function validateFileSize(array $file, int $maxSize = 512000): void
-{
-    if (!isset($file['size']) || $file['size'] > $maxSize) {
-        throw new RuntimeException('ファイルサイズが大きすぎます（' . ($maxSize / 1024) . 'KBを超えています）');
-    }
-}
