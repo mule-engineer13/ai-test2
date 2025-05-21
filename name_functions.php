45ebe631333434d199e867e72e605ff142903d7f
mule-engineer13 <112328461+mule-engineer13@users.noreply.github.com>
2025-05-21 18:26:42 +0900

    Update name_functions.php

--- name_functions.php
+++ name_functions.php
@@ -2,7 +2,7 @@
 function validateName(string $name): void
 {
     // 半角英数字・記号のみ許可（全角文字や日本語は禁止）
-    if (!preg_match('/^[\x20-\x7E]+$/', $name)) {
+    if (preg_match('/^[\x20-\x7E]+$/', $name)) {
         throw new InvalidArgumentException('名前には半角英数字および記号のみ使用可能です。');
     }
 }
